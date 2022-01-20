3ba146c9b4b9ebbddc94e7204f2374a1f76bec6c
Bernadette Kebirungi <u17109435@tuks.co.za>
2022-01-20 15:00:17 +0200

    initial commit

---
+++ todo-frontend/src/APIHelper.js
@@ -0,0 +1,44 @@
+import fetch from 'node-fetch';
+
+//just checking that this gets committed
+const API_URL = "http://localhost:3000/todos/"
+
+async function createTodo(task) {
+  const body = {task: task};
+
+  const response = await fetch(API_URL, {
+	method: 'post',
+	body: JSON.stringify(body),
+	headers: {'Content-Type': 'application/json'}
+  });
+  const data = await response.json();
+  console.log(data);
+
+  return data
+}
+
+async function deleteTodo(id) {
+    const message = await fetch(`${API_URL}${id}`, { method: 'DELETE' });
+    return message
+}
+
+
+async function updateTodo(id, payload) {
+    const requestOptions = {
+        method: 'PUT',
+        headers: { 'Content-Type': 'application/json' },
+        body: JSON.stringify({ payload })
+    };
+    const response = await fetch(`${API_URL}${id}`, requestOptions);
+    const data = await response.json();
+    return data
+}
+
+// 
+async function getAllTodos() {
+  const response = await fetch(API_URL);
+  const data = await response.json();
+  return data
+}
+
+export default { createTodo, deleteTodo, updateTodo, getAllTodos }
